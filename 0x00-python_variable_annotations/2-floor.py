86cb9dc89692961188b52599f03669b43c5c53db
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-05 19:16:22 +0300

    Fix lint errors

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,9 +4,11 @@ This is a module that provides a function for finding the floor of a float.
 """
 import math
 
+
 def floor(n: float) -> int:
     """
-    This function finds the floor of a float and returns the result as an integer.
+    This function finds the floor of a float and returns the result as an
+    integer.
 
     Parameters:
     n (float): The float to find the floor of.
